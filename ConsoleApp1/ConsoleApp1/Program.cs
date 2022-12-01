4f282a2fbbc0fc9c4a3a1b42e78b4e4335f6d4d2
Cejpa-Jakub <114338040+Cejpa-Jakub@users.noreply.github.com>
2022-12-01 12:22:38 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more informa
+using System.Collections.Generic;
+using System.Security.Cryptography;
+int points = 0;
+string CN = "yes";
+string C = CN.ToLower();
+while (C == "yes")
+{
+    for (int D = 0; D < 21;)
+    {
+        var random = new Random();
+        var card = new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11" };
+        points = random.Next(card.Count);
+        D = D + points;
+        if (D >= 21)
+        {
+            Console.WriteLine(D);
+            Console.WriteLine("if your number is 21 you won big price...");
+        }
+
+    }
+    Console.WriteLine("continue");
+    C = Console.ReadLine();
+}
+
+
+
